c7c1dd96eca08388b2371b61b44767c77c40a788
TrongVo <PhamVo@gmail.com>
2023-09-01 14:50:21 +0700

    add new project

---
+++ db/sqlc/main_test.go
@@ -0,0 +1,34 @@
+package db
+
+import (
+	"context"
+	"github.com/jackc/pgx/v5/pgxpool"
+	"log"
+	"os"
+	"simplebank/util"
+	"testing"
+
+	_ "github.com/lib/pq"
+)
+
+const (
+	dbDriver = "postgres"
+	dbSource = "postgresql://root:123456@localhost:5432/simple_bank?sslmode=disable"
+)
+
+var testStore Store
+
+func TestMain(m *testing.M) {
+	config, err := util.LoadConfig("../..")
+	if err != nil {
+		log.Fatal("cannot load config:", err)
+	}
+
+	connPool, err := pgxpool.New(context.Background(), config.DBSource)
+	if err != nil {
+		log.Fatal("cannot connect to db:", err)
+	}
+
+	testStore = NewStore(connPool)
+	os.Exit(m.Run())
+}
